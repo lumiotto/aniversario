0aa1289405710349e8625ac78cfb44df9b9f6651
Luiza Miotto <luu.miotto@gmail.com>
2023-11-14 14:31:59 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+const dataDoEvento = new Date("Jan 20, 2024 00:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+
+}, 1000);
